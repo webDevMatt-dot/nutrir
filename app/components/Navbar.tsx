763e15fe1158463c3fd09768b158281f57e6cfd9
Matthew Gift Luis <matt@Matthews-MacBook-Pro.local>
2025-12-14 13:54:25 +0200

    FAQ mobile navbar fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -127,8 +127,8 @@ export default function Navbar() {
                         <Link href="/" className="text-lg font-medium text-[#1A2621]" onClick={() => setIsMobileMenuOpen(false)}>Home</Link>
                         <Link href="/collections/all-products" className="text-lg font-medium text-[#1A2621]" onClick={() => setIsMobileMenuOpen(false)}>Shop</Link>
                         <Link href="/our-story" className="text-lg font-medium text-[#1A2621]" onClick={() => setIsMobileMenuOpen(false)}>Our Story</Link>
-                        <Link href="/faq" className="text-lg font-medium text-[#1A2621]" onClick={() => setIsMobileMenuOpen(false)}>FAQ</Link>
                         <Link href="/policies" className="text-lg font-medium text-[#1A2621]" onClick={() => setIsMobileMenuOpen(false)}>Policies</Link>
+                        <Link href="/faq" className="text-lg font-medium text-[#1A2621]" onClick={() => setIsMobileMenuOpen(false)}>FAQ</Link>
                     </div>
                 </div>
             </div>
